62049adb3af59256740f657858182048a89fb28c
jiragangel <jgangel@up.edu.ph>
2026-03-10 07:31:23 +0800

    inherit_nobility with hierarchy

--- genetics.py
+++ genetics.py
@@ -1,29 +1,32 @@
 import services # type: ignore
-from sims4.resources import Types # type: ignore
 from career_service import add_noble_career_to_sim, getNobleCareerInstance
-from sims.sim_info_types import Age
-from utils import display_all_attributes, get_full_name, write_to_log
+from utils import get_full_name, write_to_log
 
 def inherit_nobility(output_func):
     try:
         sim_manager = services.sim_info_manager()
-        for sim_info in sim_manager.get_all():
-            
-            if (sim_info.age == Age.YOUNG_ADULT):
-                parents_info = sim_info.genealogy.get_parent_sim_infos_gen()
-                display_all_attributes(sim_info.genealogy)
+        for parent_info in sim_manager.get_all():
+            parent_career = getNobleCareerInstance(parent_info)
 
-                for parent in parents_info:
-                    nobility = getNobleCareerInstance(parent)
+            if not parent_career == None:
+                children_info = parent_info.genealogy.get_child_sim_infos_gen()
 
-                    if not nobility == None:
-                        add_noble_career_to_sim(f"{sim_info.first_name} {sim_info.last_name}", output_func)
+                hierarchy = parent_career.level - 1
+                for child_info in children_info:
+                    if child_info.is_teen_or_older:
+                        child_career = getNobleCareerInstance(child_info)
 
-                        career_instance = getNobleCareerInstance(sim_info)
-
-                        if not career_instance is None:
-                            career_instance.promote(nobility.level - 1 - career_instance.level)
+                        if child_career is None:
+                            add_noble_career_to_sim(f"{get_full_name(child_info)}", output_func)
                             
-                            write_to_log(f"{get_full_name(sim_info)} is now a noble. Inherited from {get_full_name(parent)} level {nobility.user_level}")
+                        child_career = getNobleCareerInstance(child_info)
+
+                        if not child_career is None and hierarchy - child_career.level > 0:
+                            child_career.promote(hierarchy - child_career.level)
+                            
+                        write_to_log(f"{get_full_name(child_info)}({child_career.user_level}) is now a noble. Inherited from {get_full_name(parent_info)}({parent_career.user_level})")
+
+                    hierarchy = hierarchy - 1
+
     except Exception as e:
         output_func(f"Error: {e}")
